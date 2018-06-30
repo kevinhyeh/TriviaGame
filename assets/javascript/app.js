e45bb379c6cd8aa5ad287ae0c9ba64d2581499ed
Kevin Yeh <kevinyeh@Kevins-MacBook-Pro-3.local>
2018-06-30 09:26:50 -0700

    added css styles(fonts, images); created functions to reduce js code;

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,114 +1,164 @@
 var answers = [
-    ['One', 'Two', 'Three', 'Four'],
-    ['Uno', 'Dos', 'Tres', 'Cuatro'],
-    ['Yut', 'Eee', 'Sam', 'Say'],
-    ['Itchi', 'Nee', 'San', 'Shi']
+    ['Mercury', 'Jupiter', 'Earth', 'Mars'],
+    ['225 Days', '324 Days', '365 Days', '408 Days'],
+    ['Edwin Aldrin', 'Michael Collins', 'Lance Armstrong', 'Neil Armstrong'],
+    ['5,349 Kelvin', '5,778 Kelvin', '4,197 Kelvin', '6,304 Kelvin'],
+    ['Venus', 'Saturn', 'Sun', 'Mars'],
+    ['2005', '2006', '2007', '2008'],
+    ['2', '34', '53', '78'],
+    ['About 100 Days', 'About 200 Days', 'About 300 Days', 'About 400 Days'],
+    ['Neptune', 'Planet Nine', 'Xena', '9th Planet'],
+    ['1,479 Minutes', '1,351 Minutes', '1,589 Minutes', '1,154 Minutes']
 ];
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unanswered = 0;
-var sTimer = 30;
+var sTimer = 15;
 var nextTimer = 0;
 
 function answersOptions(e) {
-    for (var i in answers) {
+    for (var i in answers[0]) {
         var box = $('<div>')
         var button = $('<button>').html(answers[e][i]);
         button.addClass('options');
         box.html(button);
-        $('.box').append(box);
+        $('#box').append(box);
     }
 }
 
 function startTimer() {
+	var trueAns = $('[data-answer=true]').html();
     sTimer--;
-    $('.timer').html(sTimer);
+    $('#timer').html('Time Remaining: ' + sTimer + ' seconds');
     if (sTimer == 0) {
-        $('.question').html("");
-        $('.box').html("Ran out of time");
+        $('#question').html("");
+        $('#box').html("Ran out of time");
         clearInterval(clearTimer);
         invisTimer = setInterval(interTimer, 1000);
         unanswered++;
+        $('#gif').html("The correct answer is " + trueAns);
     }
+}
+
+function nextQuestion(next) {
+    sTimer = 15;
+    $('#box').empty();
+    answersOptions(next);
+    $('#timer').html('Time Remaining: ' + sTimer + ' seconds');
+    clearTimer = setInterval(startTimer, 1000);
+    clearInterval(invisTimer);
+    $('#gif').empty();
 }
 
 function interTimer() {
     nextTimer++;
     if (nextTimer == 3) {
-        sTimer = 30
-        answersOptions(1);
-        $('.timer').html(sTimer);
-        clearTimer = setInterval(startTimer, 1000);
-        clearInterval(invisTimer);
-        $('#gif').empty();
-        $('button:eq(1)').attr('data-answer', true);
+        $('#question').html('How many days does the Earth orbit the Sun?');
+        nextQuestion(1);
+        $('button:eq(3)').attr('data-answer', true);
     }
     if (nextTimer == 6) {
-        sTimer = 30
-        answersOptions(2);
-        $('.timer').html(sTimer);
-        clearTimer = setInterval(startTimer, 1000);
-        clearInterval(invisTimer);
-        $('#gif').empty();
-        $('button:eq(1)').attr('data-answer', true);
+        $('#question').html('Who was the first American to walk on the Moon?');
+        nextQuestion(2);
+        $('button:eq(4)').attr('data-answer', true);
     }
     if (nextTimer == 9) {
-        sTimer = 30
-        answersOptions(3);
-        $('.timer').html(sTimer);
-        clearTimer = setInterval(startTimer, 1000);
-        clearInterval(invisTimer);
-        $('#gif').empty();
-        $('button:eq(1)').attr('data-answer', true);
+        $('#question').html('How hot is the Sun?');
+        nextQuestion(3);
+        $('button:eq(2)').attr('data-answer', true);
     }
     if (nextTimer == 12) {
+        $('#question').html('Which is the hottest planet?');
+        nextQuestion(4);
+        $('button:eq(1)').attr('data-answer', true);
+    }
+    if (nextTimer == 15) {
+        $('#question').html('When did Pluto become a dwarf planet?');
+        nextQuestion(5);
+        $('button:eq(2)').attr('data-answer', true);
+    }
+    if (nextTimer == 18) {
+        $('#question').html('How many moons does Jupiter have?');
+        nextQuestion(6);
+        $('button:eq(3)').attr('data-answer', true);
+    }
+    if (nextTimer == 21) {
+        $('#question').html('How long does it take to get to Mars?');
+        nextQuestion(7);
+        $('button:eq(3)').attr('data-answer', true);
+    }
+    if (nextTimer == 24) {
+        $('#question').html('As of 2016, which one is the 9th planet?');
+        nextQuestion(8);
+        $('button:eq(2)').attr('data-answer', true);
+    }
+    if (nextTimer == 27) {
+        $('#question').html('How long is a Martian day?');
+        nextQuestion(9);
+        $('button:eq(1)').attr('data-answer', true);
+    }
+    if (nextTimer == 30) {
         var restart = $('<button>').html("Restart");
-        var result = $('<div>').html(correctAnswers + '<br>' + incorrectAnswers + '<br>' + unanswered);
+        var result = $('<div>').html('Correct Answers: ' + correctAnswers + '<br>' + 'Incorrect Answers: ' + incorrectAnswers + '<br>' + 'Unanswered: ' + unanswered);
         restart.addClass('restart');
         $('#gif').html(restart);
-        $('.question').html(result);
+        $('#question').html(result);
         clearInterval(invisTimer);
-        $('.box').empty();
+        $('#box').empty();
     }
 }
 
-$('.start').on('click', function() {
-    $('.timer').html(sTimer);
+$('#start').on('click', function() {
+    $('#timer').html('Time Remaining: ' + sTimer + ' seconds');
     clearTimer = setInterval(startTimer, 1000);
     $(this).hide();
-    $('.question').html('What language is this?');
+    $('#question').html('Which planet is the closest to the sun?');
     answersOptions(0);
     $('button:eq(1)').attr('data-answer', true);
 });
 
 $(document).on('click', '.options', function() {
-    // debugger;
-    // || $(this).html() == 'Uno' || $(this).html() == 'Yut' || $(this).html() == 'Itchi'
-    if ($(this).html() == 'One' || $(this).html() == 'Uno' || $(this).html() == 'Yut' || $(this).html() == 'Itchi') {
-        if ($(this).html() == 'One') {
-            $('#gif').html("one");
+    if ($(this).html() == 'Mercury' || $(this).html() == '365 Days' || $(this).html() == 'Neil Armstrong' || $(this).html() == '5,778 Kelvin' || $(this).html() == 'Venus' || $(this).html() == '2006' || $(this).html() == '53' || $(this).html() == 'About 300 Days' || $(this).html() == 'Planet Nine' || $(this).html() == '1,479 Minutes') {
+        if ($(this).html() == 'Mercury') {
+            $('#gif').html("Gif of Mercury");
         }
-        if ($(this).html() == 'Uno') {
-            $('#gif').html("uno");
+        if ($(this).html() == '365 Days') {
+            $('#gif').html("Gif of Calender");
         }
-        if ($(this).html() == 'Yut') {
-            $('#gif').html("yut");
+        if ($(this).html() == 'Neil Armstrong') {
+            $('#gif').html("Gif of Neil");
         }
-        if ($(this).html() == 'Itchi') {
-            $('#gif').html("itchi");
+        if ($(this).html() == '5,778 Kelvin') {
+            $('#gif').html("Gif of the Sun");
+        }
+        if ($(this).html() == 'Venus') {
+            $('#gif').html("Gif of Venus");
+        }
+        if ($(this).html() == '2006') {
+            $('#gif').html("Gif of Pluto");
+        }
+        if ($(this).html() == '53') {
+            $('#gif').html("Gif of Jupiter");
+        }
+        if ($(this).html() == 'About 300 Days') {
+            $('#gif').html("Gif of space travel");
+        }
+        if ($(this).html() == 'Planet Nine') {
+            $('#gif').html("Gif of Planet Nine");
+        }
+        if ($(this).html() == '1479 Minutes') {
+            $('#gif').html("Gift of a martian");
         }
         clearInterval(clearTimer);
-        $('.question').html("");
-        $('.box').html("Correct");
+        $('#question').html("Correct");
         invisTimer = setInterval(interTimer, 1000);
         correctAnswers++;
     } else {
-        var trueAns = $('[data-answer=true]').html();
+        trueAns = $('[data-answer=true]').html();
         clearInterval(clearTimer);
-        $('.question').html("");
-        $('.box').html("Incorrect");
+        $('#question').html("Incorrect");
         invisTimer = setInterval(interTimer, 1000);
-        $('#gif').html("correct answer is " + trueAns)
+        $('#gif').html("The correct answer is " + trueAns);
         incorrectAnswers++;
     }
 });
@@ -119,20 +169,9 @@ $(document).on('click', '.restart', function() {
     unanswered = 0;
     sTimer = 30;
     nextTimer = 0;
-    $('.question').html('What language is this?');
+    $('#question').html('What language is this?');
     answersOptions(0);
-    $('.timer').html(sTimer);
+    $('#timer').html(sTimer);
     clearTimer = setInterval(startTimer, 1000);
-    $('#gif').empty();
-    
+    $('#gif').empty();	
 });
-
-
-
-
-
-
-
-
-
-
